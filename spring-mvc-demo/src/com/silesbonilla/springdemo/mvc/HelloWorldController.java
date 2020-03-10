11c524f0c61596f1ea4dc09810ed75b899faf4fd
Trethtzer <guille@silesbonilla.com>
2020-03-10 12:39:37 +0100

    Spring MVC - Request Params and Request Mappings

--- spring-mvc-demo/src/com/silesbonilla/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/silesbonilla/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	@RequestMapping("/showForm")
@@ -26,6 +28,19 @@ public class HelloWorldController {
 		theName = theName.toUpperCase();
 		
 		String result = "Yo! " + theName;
+		
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {		
+		String theName = name;
+		
+		theName = theName.toUpperCase();
+		
+		String result = "Yepppp " + theName;
 		
 		model.addAttribute("message", result);
 		
