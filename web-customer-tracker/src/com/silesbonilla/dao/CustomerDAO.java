fd171e3f996911c9a7a690d25fc34d09ffaf206b
Trethtzer <guille@silesbonilla.com>
2020-04-01 18:21:38 +0200

    Build a database web app - Spring MVC and hibernate project

---
+++ web-customer-tracker/src/com/silesbonilla/dao/CustomerDAO.java
@@ -0,0 +1,12 @@
+package com.silesbonilla.dao;
+
+import java.util.List;
+
+import com.silesbonilla.entities.Customer;
+
+public interface CustomerDAO {
+
+	
+	public List<Customer> getCustomers();
+
+}
