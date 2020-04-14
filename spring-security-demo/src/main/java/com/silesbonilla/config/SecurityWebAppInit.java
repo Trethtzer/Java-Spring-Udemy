948ba1b436462cb3979299322f71540ffb2a1feb
Trethtzer <guille@silesbonilla.com>
2020-04-14 14:12:02 +0200

    Spring security - getting started

---
+++ spring-security-demo/src/main/java/com/silesbonilla/config/SecurityWebAppInit.java
@@ -0,0 +1,9 @@
+package com.silesbonilla.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInit extends AbstractSecurityWebApplicationInitializer{
+	
+	
+	
+}
